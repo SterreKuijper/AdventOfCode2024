b157d9727bbdec06586f0a654af8a5b0ea7eb57c
Sterre <sterrekuijper@gmail.com>
2024-12-06 15:02:09 -0500

    Day 6 Part 2 Unsolved

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -15,6 +15,10 @@ function findGuard(grid: string[]) {
     return [0, 0];
 }
 
+function findInArray(array: number[][], element: number[]) {
+    return array.find(el => el[0] === element[0] && el[1] === element[1]);
+}
+
 function getNextCoordinates(currentCoord: number[], direction: number[]) {
     return [currentCoord[0] + direction[0], currentCoord[1] + direction[1]];
 }
@@ -79,6 +83,67 @@ function partOne(input: string) {
 
 function partTwo(input: string) {
     let result = 0;
+    // create the map
+    const map = input.split('\n');
+    map.forEach((line, index) => {
+        map[index] = line.trim()
+    });
+
+    // get the guard starting position
+    let guardStartCoord = findGuard(map);
+
+    // create the direction orientation
+    const directions = [
+        [0, -1],
+        [1, 0],
+        [0, 1],
+        [-1, 0]
+    ];
+    let startDirection = directions[0];
+
+    // loop over the map
+    // to try out all the possible paths
+    const width = map[0].length;
+    const height = map.length;
+    for (let j = 0; j < height; j++) {
+        for (let i = 0; i < width; i++) {
+            // if the current position is empty an obstacle can be placed
+            if (map[j][i] === '.') {
+                // create a deep copy of the map to place the obstacle
+                let cloneMap = structuredClone(map);
+                cloneMap[j] = cloneMap[j].substring(0, i) + '#' + cloneMap[j].substring(i + 1);
+
+                // starting coordinates for the guard
+                let guardCoord = guardStartCoord;
+                let direction = startDirection;
+                let visited = [guardCoord];
+                let alreadyVisited = [guardCoord];
+
+                while (true) {
+                    const nextCoord = getNextCoordinates(guardCoord, direction);
+                    const nextPosition = getPosition(cloneMap, nextCoord);
+                    if (nextPosition === undefined) {
+                        break;
+                    } else {
+                        if (nextPosition === '#') {
+                            direction = turnRight(direction);
+                        } else {
+                            if (!findInArray(visited, nextCoord)) {
+                                visited.push(nextCoord);
+                            } else {
+                                alreadyVisited.push(nextCoord);
+                                if (alreadyVisited.length > 20) {
+                                    result++;
+                                    break;
+                                }
+                            }
+                            guardCoord = nextCoord;
+                        }
+                    }
+                }
+            }
+        }
+    }
     return result;
 }
 

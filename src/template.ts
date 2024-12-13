d4eaaf506db3b895de3d1ffae317c20ac8e2ea0c
Sterre <sterrekuijper@gmail.com>
2024-12-13 16:07:06 +0100

    Added timer

--- src/template.ts
+++ src/template.ts
@@ -2,6 +2,7 @@ import {readFile} from "./utils/readFile";
 
 const input = readFile('../inputs/dayXX.txt');
 
+
 function partOne(input: string) {
     let result = 0;
     return result;
@@ -12,5 +13,9 @@ function partTwo(input: string) {
     return result;
 }
 
+console.time('part1')
 console.log('Part 1:', partOne(input));
+console.timeEnd('part1')
+console.time('part2')
 console.log('Part 2:', partTwo(input));
+console.timeEnd('part2')
